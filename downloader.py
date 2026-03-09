9c04bcb84cfde38be6107f944352c455d06b302e
abdullohasqarov202-glitch <abdullohasqarov202@gmail.com>
2026-03-09 22:11:41 +0300

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -1,31 +1,38 @@
 import yt_dlp
 
 def download_video(url):
+
     ydl_opts = {
-        "format": "best",
+        "format": "bestvideo+bestaudio/best",
         "outtmpl": "video.%(ext)s",
-        "noplaylist": True
+        "noplaylist": True,
+        "quiet": True,
+        "nocheckcertificate": True
     }
 
     try:
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info = ydl.extract_info(url, download=True)
-            return ydl.prepare_filename(info)
+            file = ydl.prepare_filename(info)
+            return file
     except Exception as e:
         print(e)
         return None
 
 
 def download_audio(url):
+
     ydl_opts = {
         "format": "bestaudio/best",
-        "outtmpl": "audio.%(ext)s"
+        "outtmpl": "audio.%(ext)s",
+        "quiet": True
     }
 
     try:
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info = ydl.extract_info(url, download=True)
-            return ydl.prepare_filename(info)
+            file = ydl.prepare_filename(info)
+            return file
     except Exception as e:
         print(e)
         return None
