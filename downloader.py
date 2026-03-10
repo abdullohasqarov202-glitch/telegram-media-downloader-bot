20961470e496e13fc6cba4263127e6771478f674
abdullohasqarov202-glitch <abdullohasqarov202@gmail.com>
2026-03-10 10:19:05 +0300

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -1,6 +1,7 @@
 import yt_dlp
 import uuid
 
+
 def download_video(url):
 
     filename = f"video_{uuid.uuid4().hex}.%(ext)s"
@@ -17,6 +18,27 @@ def download_video(url):
                 "player_client": ["android"]
             }
         }
+    }
+
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(url, download=True)
+            file = ydl.prepare_filename(info)
+            return file
+    except Exception as e:
+        print(e)
+        return None
+
+
+def download_audio(url):
+
+    filename = f"audio_{uuid.uuid4().hex}.%(ext)s"
+
+    ydl_opts = {
+        "format": "bestaudio",
+        "outtmpl": filename,
+        "quiet": True,
+        "noplaylist": True
     }
 
     try:
