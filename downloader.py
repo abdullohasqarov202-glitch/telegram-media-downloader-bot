957db45e081c83a9ef05061262af86152472681d
abdullohasqarov202-glitch <abdullohasqarov202@gmail.com>
2026-03-10 10:13:47 +0300

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -1,17 +1,14 @@
 import yt_dlp
 
-
 def download_video(url):
 
     ydl_opts = {
-        "format": "bestvideo+bestaudio/best",
+        "format": "best[ext=mp4]/best",
         "outtmpl": "video.%(ext)s",
-        "merge_output_format": "mp4",
         "noplaylist": True,
         "quiet": True,
         "nocheckcertificate": True,
 
-        # YouTube blok bo'lmasligi uchun
         "extractor_args": {
             "youtube": {
                 "player_client": ["android"]
@@ -32,22 +29,16 @@ def download_video(url):
 def download_audio(url):
 
     ydl_opts = {
-        "format": "bestaudio/best",
+        "format": "bestaudio",
         "outtmpl": "audio.%(ext)s",
-        "quiet": True,
-
-        # MP3 ga o‘tkazish
-        "postprocessors": [{
-            "key": "FFmpegExtractAudio",
-            "preferredcodec": "mp3",
-            "preferredquality": "192"
-        }]
+        "quiet": True
     }
 
     try:
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            ydl.download([url])
-        return "audio.mp3"
+            info = ydl.extract_info(url, download=True)
+            file = ydl.prepare_filename(info)
+            return file
     except Exception as e:
         print(e)
         return None
