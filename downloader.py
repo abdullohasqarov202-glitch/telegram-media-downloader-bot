9625d3b656e753cbf7a639999ba0637f82ed0d0c
abdullohasqarov202-glitch <abdullohasqarov202@gmail.com>
2026-03-12 00:32:51 +0300

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -1,43 +1,28 @@
 import yt_dlp
-import uuid
-
 
 def download_video(url):
-
-    filename = f"{uuid.uuid4().hex}.%(ext)s"
-
     ydl_opts = {
-        "format": "best",
-        "outtmpl": filename,
-        "quiet": True,
-        "noplaylist": True,
+        'format': 'best',
+        'outtmpl': 'video.%(ext)s'
     }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        info = ydl.extract_info(url)
-        file = ydl.prepare_filename(info)
-
-    return file
+        info = ydl.extract_info(url, download=True)
+        return ydl.prepare_filename(info)
 
 
 def download_audio(url):
-
-    filename = f"{uuid.uuid4().hex}.%(ext)s"
-
     ydl_opts = {
-        "format": "bestaudio/best",
-        "outtmpl": filename,
-        "quiet": True,
-
-        "postprocessors": [{
-            "key": "FFmpegExtractAudio",
-            "preferredcodec": "mp3",
-            "preferredquality": "192",
+        'format': 'bestaudio/best',
+        'outtmpl': 'audio.%(ext)s',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192'
         }]
     }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        info = ydl.extract_info(url)
-        file = ydl.prepare_filename(info)
+        ydl.extract_info(url, download=True)
 
-    return file.replace(".webm", ".mp3").replace(".m4a", ".mp3")
+    return "audio.mp3"
