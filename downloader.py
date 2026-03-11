9a2c9eebe6512015a0bd24970f300ddfc5301e04
abdullohasqarov202-glitch <abdullohasqarov202@gmail.com>
2026-03-11 21:19:02 +0300

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -1,18 +1,20 @@
 import yt_dlp
 import uuid
 
-def download_video(url):
+
+# VIDEO YUKLASH (SIFAT BILAN)
+def download_video(url, quality="720"):
 
     filename = f"video_{uuid.uuid4().hex}.%(ext)s"
 
     ydl_opts = {
-        "format": "best",
+        "format": f"bestvideo[height<={quality}]+bestaudio/best",
         "outtmpl": filename,
         "quiet": True,
         "noplaylist": True,
         "nocheckcertificate": True,
         "ignoreerrors": True,
-        "cookiefile": "cookies.txt",
+        "merge_output_format": "mp4",
 
         "extractor_args": {
             "youtube": {
@@ -22,11 +24,21 @@ def download_video(url):
     }
 
     try:
+
+        # agar link bo'lmasa qo'shiq qidiradi
+        if not url.startswith("http"):
+            url = f"ytsearch1:{url}"
+
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+
             info = ydl.extract_info(url, download=True)
 
             if not info:
                 return None
+
+            # search bo'lsa
+            if "entries" in info:
+                info = info["entries"][0]
 
             return ydl.prepare_filename(info)
 
@@ -35,26 +47,42 @@ def download_video(url):
         return None
 
 
+# MP3 YUKLASH
 def download_audio(url):
 
-    filename = f"audio_{uuid.uuid4().hex}.%(ext)s"
+    filename = f"audio_{uuid.uuid4().hex}.mp3"
 
     ydl_opts = {
-        "format": "bestaudio",
+        "format": "bestaudio/best",
         "outtmpl": filename,
         "quiet": True,
         "noplaylist": True,
-        "cookiefile": "cookies.txt"
+
+        "postprocessors": [
+            {
+                "key": "FFmpegExtractAudio",
+                "preferredcodec": "mp3",
+                "preferredquality": "192",
+            }
+        ]
     }
 
     try:
+
+        if not url.startswith("http"):
+            url = f"ytsearch1:{url}"
+
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+
             info = ydl.extract_info(url, download=True)
 
             if not info:
                 return None
 
-            return ydl.prepare_filename(info)
+            if "entries" in info:
+                info = info["entries"][0]
+
+            return filename
 
     except Exception as e:
         print(e)
