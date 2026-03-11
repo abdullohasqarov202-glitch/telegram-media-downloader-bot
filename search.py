67378db203a4113fc96102d704a59f0aaeed2b9d
abdullohasqarov202-glitch <abdullohasqarov202@gmail.com>
2026-03-11 23:29:42 +0300

    Update search.py

--- search.py
+++ search.py
@@ -5,13 +5,23 @@ def search_song(query):
 
     ydl_opts = {
         "quiet": True,
-        "default_search": "ytsearch1"
+        "default_search": "ytsearch5"
     }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
 
         info = ydl.extract_info(query, download=False)
 
-        video = info["entries"][0]
+        results = []
 
-        return video["webpage_url"], video["title"]
+        if "entries" not in info:
+            return results
+
+        for video in info["entries"]:
+
+            results.append({
+                "title": video["title"],
+                "url": video["webpage_url"]
+            })
+
+        return results
