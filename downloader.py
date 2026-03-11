58f44adaaf896b08c520d3f08b9c62c50e141f90
abdullohasqarov202-glitch <abdullohasqarov202@gmail.com>
2026-03-11 23:17:35 +0300

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -2,18 +2,7 @@ import yt_dlp
 import uuid
 
 
-def fix_url(url):
-
-    if "youtube.com/shorts/" in url:
-        video_id = url.split("shorts/")[1].split("?")[0]
-        return f"https://www.youtube.com/watch?v={video_id}"
-
-    return url
-
-
 def download_video(url):
-
-    url = fix_url(url)
 
     filename = f"{uuid.uuid4().hex}.mp4"
 
@@ -21,18 +10,10 @@ def download_video(url):
         "format": "best",
         "outtmpl": filename,
         "quiet": True,
-
+        "noplaylist": True,
         "http_headers": {
             "User-Agent": "Mozilla/5.0"
-        },
-
-        "extractor_args": {
-            "youtube": {
-                "player_client": ["android", "web"]
-            }
-        },
-
-        "noplaylist": True
+        }
     }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
@@ -43,15 +24,11 @@ def download_video(url):
 
 def download_audio(url):
 
-    url = fix_url(url)
-
     filename = f"{uuid.uuid4().hex}.mp3"
 
     ydl_opts = {
-        "format": "bestaudio",
+        "format": "bestaudio/best",
         "outtmpl": filename,
-        "ffmpeg_location": "/usr/bin/ffmpeg",
-
         "postprocessors": [{
             "key": "FFmpegExtractAudio",
             "preferredcodec": "mp3",
