b42a9e33a1eed4ca4dd8f33f788a0090cde2841d
abdullohasqarov202-glitch <abdullohasqarov202@gmail.com>
2026-03-11 21:58:03 +0300

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -1,6 +1,7 @@
 import yt_dlp
 import uuid
 
+
 def download_video(url):
 
     filename = f"video_{uuid.uuid4().hex}.%(ext)s"
@@ -11,18 +12,13 @@ def download_video(url):
         "quiet": True,
         "noplaylist": True,
         "nocheckcertificate": True,
-        "ignoreerrors": True,
-        "cookiefile": "cookies.txt",
-
-        "extractor_args": {
-            "youtube": {
-                "player_client": ["android"]
-            }
-        }
+        "ignoreerrors": True
     }
 
     try:
+
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+
             info = ydl.extract_info(url, download=True)
 
             if not info:
@@ -35,26 +31,34 @@ def download_video(url):
         return None
 
 
+
 def download_audio(url):
 
-    filename = f"audio_{uuid.uuid4().hex}.%(ext)s"
+    filename = f"audio_{uuid.uuid4().hex}.mp3"
 
     ydl_opts = {
         "format": "bestaudio",
         "outtmpl": filename,
         "quiet": True,
         "noplaylist": True,
-        "cookiefile": "cookies.txt"
+
+        "postprocessors": [{
+            "key": "FFmpegExtractAudio",
+            "preferredcodec": "mp3",
+            "preferredquality": "192"
+        }]
     }
 
     try:
+
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+
             info = ydl.extract_info(url, download=True)
 
             if not info:
                 return None
 
-            return ydl.prepare_filename(info)
+            return filename
 
     except Exception as e:
         print(e)
