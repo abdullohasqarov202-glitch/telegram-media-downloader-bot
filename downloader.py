6139e2ad096042c836d982fe3ef9768b06d78626
abdullohasqarov202-glitch <abdullohasqarov202@gmail.com>
2026-03-12 06:35:42 +0300

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -3,7 +3,8 @@ import yt_dlp
 def download_video(url):
     ydl_opts = {
         'format': 'best',
-        'outtmpl': 'video.%(ext)s'
+        'outtmpl': 'video.%(ext)s',
+        'ffmpeg_location': '/usr/bin/ffmpeg'
     }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
@@ -15,6 +16,7 @@ def download_audio(url):
     ydl_opts = {
         'format': 'bestaudio/best',
         'outtmpl': 'audio.%(ext)s',
+        'ffmpeg_location': '/usr/bin/ffmpeg',
         'postprocessors': [{
             'key': 'FFmpegExtractAudio',
             'preferredcodec': 'mp3',
@@ -23,6 +25,6 @@ def download_audio(url):
     }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        ydl.extract_info(url, download=True)
+        info = ydl.extract_info(url, download=True)
 
     return "audio.mp3"
