919a4951391005e890fb30a15b67548c25adbbb0
abdullohasqarov202-glitch <abdullohasqarov202@gmail.com>
2026-03-12 07:20:41 +0300

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -1,36 +1,34 @@
 import yt_dlp
 import uuid
 
+
 def download_video(url):
 
-    filename = f"video_{uuid.uuid4().hex}.%(ext)s"
+    filename = f"video_{uuid.uuid4().hex}.mp4"
 
     ydl_opts = {
-        "format": "bv*+ba/b",
         "outtmpl": filename,
-        "merge_output_format": "mp4"
+        "format": "best",
+        "noplaylist": True
     }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        info = ydl.extract_info(url, download=True)
-        return ydl.prepare_filename(info)
+        ydl.download([url])
+
+    return filename
 
 
 def download_audio(url):
 
-    filename = f"audio_{uuid.uuid4().hex}.%(ext)s"
+    filename = f"audio_{uuid.uuid4().hex}.mp3"
 
     ydl_opts = {
-        "format": "bestaudio/best",
         "outtmpl": filename,
-        "postprocessors": [{
-            "key": "FFmpegExtractAudio",
-            "preferredcodec": "mp3",
-            "preferredquality": "192"
-        }]
+        "format": "bestaudio",
+        "noplaylist": True
     }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        ydl.extract_info(url, download=True)
+        ydl.download([url])
 
-    return filename.replace("%(ext)s", "mp3")
+    return filename
