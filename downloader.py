37dfbc266829a7808ba4e9f3ee7063662f70dbae
abdullohasqarov202-glitch <abdullohasqarov202@gmail.com>
2026-03-12 06:58:43 +0300

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -1,34 +1,61 @@
 import yt_dlp
+import uuid
 
 def download_video(url):
 
+    filename = f"video_{uuid.uuid4().hex}.%(ext)s"
+
     ydl_opts = {
-        'format': 'best',
-        'outtmpl': 'video.%(ext)s'
+        "format": "best",
+        "outtmpl": filename,
+        "quiet": True,
+        "noplaylist": True,
+        "nocheckcertificate": True,
+        "ignoreerrors": True,
+        "cookiefile": "cookies.txt",
+
+        "extractor_args": {
+            "youtube": {
+                "player_client": ["android"]
+            }
+        }
     }
 
     try:
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info = ydl.extract_info(url, download=True)
+
+            if not info:
+                return None
+
             return ydl.prepare_filename(info)
-    except:
+
+    except Exception as e:
+        print(e)
         return None
 
 
 def download_audio(url):
 
+    filename = f"audio_{uuid.uuid4().hex}.%(ext)s"
+
     ydl_opts = {
-        'format': 'bestaudio/best',
-        'outtmpl': 'audio.%(ext)s',
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'mp3'
-        }]
+        "format": "bestaudio",
+        "outtmpl": filename,
+        "quiet": True,
+        "noplaylist": True,
+        "cookiefile": "cookies.txt"
     }
 
     try:
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info = ydl.extract_info(url, download=True)
-            return ydl.prepare_filename(info).replace(".webm", ".mp3")
-    except:
+
+            if not info:
+                return None
+
+            return ydl.prepare_filename(info)
+
+    except Exception as e:
+        print(e)
         return None
