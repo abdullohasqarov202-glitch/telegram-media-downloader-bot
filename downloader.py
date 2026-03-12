f8fa983c628fd4d7fcf198ac908875639d23941b
abdullohasqarov202-glitch <abdullohasqarov202@gmail.com>
2026-03-12 06:44:20 +0300

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -1,30 +1,61 @@
 import yt_dlp
+import uuid
 
 def download_video(url):
+
+    filename = f"video_{uuid.uuid4().hex}.%(ext)s"
+
     ydl_opts = {
-        'format': 'best',
-        'outtmpl': 'video.%(ext)s',
-        'ffmpeg_location': '/usr/bin/ffmpeg'
+        "format": "best",
+        "outtmpl": filename,
+        "quiet": True,
+        "noplaylist": True,
+        "nocheckcertificate": True,
+        "ignoreerrors": True,
+        "cookiefile": "cookies.txt",
+
+        "extractor_args": {
+            "youtube": {
+                "player_client": ["android"]
+            }
+        }
     }
 
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        info = ydl.extract_info(url, download=True)
-        return ydl.prepare_filename(info)
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(url, download=True)
+
+            if not info:
+                return None
+
+            return ydl.prepare_filename(info)
+
+    except Exception as e:
+        print(e)
+        return None
 
 
 def download_audio(url):
+
+    filename = f"audio_{uuid.uuid4().hex}.%(ext)s"
+
     ydl_opts = {
-        'format': 'bestaudio/best',
-        'outtmpl': 'audio.%(ext)s',
-        'ffmpeg_location': '/usr/bin/ffmpeg',
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'mp3',
-            'preferredquality': '192'
-        }]
+        "format": "bestaudio",
+        "outtmpl": filename,
+        "quiet": True,
+        "noplaylist": True,
+        "cookiefile": "cookies.txt"
     }
 
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        info = ydl.extract_info(url, download=True)
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(url, download=True)
 
-    return "audio.mp3"
+            if not info:
+                return None
+
+            return ydl.prepare_filename(info)
+
+    except Exception as e:
+        print(e)
+        return None
